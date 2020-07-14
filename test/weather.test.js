f77f848b8fceb3f51afc78cd79768d9b03f88344
Alameen688 <ogundiran12@gmail.com>
2020-07-14 16:04:05 +0100

    Add tests for weather args check

--- test/weather.test.js
+++ test/weather.test.js
@@ -1,7 +1,18 @@
 const test = require('ava')
+const sinon = require('sinon')
+const runApp = require('../weather')
+const { log } = require('../utils')
 
 test.todo('app should run successfully when called with correct args')
 
-test.todo('app should log error text when called with no args')
+test('app should log error text when called with no location args is passed', (t) => {
+  const errorStub = sinon.stub(log, 'error')
+
+  runApp()
+  
+  t.true(
+    errorStub.calledWith('You need to pass one or more locations seperated by comma')
+  )
+})
 
 test.todo('app should log error text when called with invalid args')
