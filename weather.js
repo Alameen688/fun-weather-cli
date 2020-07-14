3be26b6b522d6959ec96970fcc99c46f4c0b8651
Alameen688 <ogundiran12@gmail.com>
2020-07-14 16:03:29 +0100

    Check if location args are passed via command

--- weather.js
+++ weather.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
+const { log } = require('./utils')
 
-const runApp = () => {
-  const [executor, ignoredBin, ...args] = process.argv
-  
+const runApp = (args = []) => {
+  if (!args.length) {
+    return log.error(
+      'You need to pass one or more locations seperated by comma'
+    )
+  }
+
   const action = args[0].split(', ')
 
   // Perform weather request
 }
 
-runApp()
+const [executor, ignoredBin, ...args] = process.argv
+
+runApp(args)
+
+module.exports = runApp
