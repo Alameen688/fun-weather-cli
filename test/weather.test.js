4d55c4002daec7022d4224f91fdb193cf4947a28
Alameen688 <ogundiran12@gmail.com>
2020-07-14 15:36:06 +0100

    Add app test case

---
+++ test/weather.test.js
@@ -0,0 +1,7 @@
+const test = require('ava')
+
+test.todo('app should run successfully when called with correct args')
+
+test.todo('app should log error text when called with no args')
+
+test.todo('app should log error text when called with invalid args')
