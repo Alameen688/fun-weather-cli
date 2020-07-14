48e028f83a97682dff2f26e9839d7f854ce282dc
Alameen688 <ogundiran12@gmail.com>
2020-07-14 16:32:47 +0100

    Add tests for table data output

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,36 @@
 const test = require('ava')
 const utils = require('../utils')
 
-test.todo('utils.generateTableFromData should output correct table info from weather data')
+test('utils.generateTableFromData should output correct table info from weather data', (t) => {
+  const data = {
+    current: {
+      humidity: 81,
+      observationTime: '09:40 PM',
+      pressure: 1009,
+      temperature: 25,
+      weatherDescriptions: ['Partly cloudy']
+    },
+    request: {
+      query: 'Tokyo, Japan'
+    }
+  }
 
-test.todo('utils.getMessageFromData should return the correct message from weather data')
+  const table = utils.generateTableFromData(data)
+  const tableValues = Object.values(table)
+
+  t.true(tableValues.some((value) => value.temperature === 25))
+  t.true(tableValues.some((value) => value.pressure === 1009))
+  t.true(tableValues.some((value) => value.humidity === 81))
+})
+
+test('utils.getMessageFromData should return the correct message from weather data', (t) => {
+  const data = {
+    weatherDescription: 'Partly cloudy',
+    emoji: '🌥️',
+    location: 'New York',
+    observationTime: '10:00 PM'
+  }
+
+  const message = utils.getMessageFromData(data)
+  t.is(message, "It's partly cloudy 🌥️  in New York at 10:00 PM")
+})
